49ae7956c5d4be1ca7a9cdf864da00daa608531e
N.Ugochukwu <115807244+Nugochukwu@users.noreply.github.com>
2024-10-23 19:21:34 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const toTop = document.querySelector(".to-top");
-
+const headings = document.querySelectorAll(".feature-box");
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 1) {
         toTop.classList.add("active");
@@ -8,3 +8,20 @@ window.addEventListener("scroll", () => {
     }
 
 })
+
+window.addEventListener("scroll", () => {
+    headings.forEach((box, index) => {
+        if (window.pageYOffset < 500) {
+            box.classList.add("active");
+            box.classList.add('show');
+        } else {
+            box.classList.remove("active");
+            box.classList.remove('show');
+        }
+        
+        setTimeout(() => {
+            box.classList.add('show');
+        }, index * 500); // Add a delay for each heading
+    });
+});
+    
